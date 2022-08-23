60d4e1a488418664535c38baf6fcbbcdea3b847e
Carlos Bridi <carlosbridi@gmail.com>
2022-08-23 13:28:35 -0300

    Controls structure

---
+++ 11 - controls structure/controls-structure.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Estruturas de controle")
+
+	numero := 10
+
+	if numero > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor que 15")
+	}
+
+	if outroNumero := numero; outroNumero > 0 {
+		fmt.Println("Número maior que zero")
+	}
+
+}

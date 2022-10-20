76ad2c4515b1db4778dac1825a4e05059d1714f3
carlosbridi <carlosbridi@gmail.com>
2022-10-20 08:42:29 -0300

    HTML render variables

---
+++ 22 - html/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		u := usuario{"João", "João@gmail.com"}
+
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
